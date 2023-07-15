409401bf4dcb5e7c01dbee01cb718054c72f79a9
Stanley Chung <98046927+aldStanley@users.noreply.github.com>
2023-07-15 18:17:09 +0800

    Finished all functions

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,25 +13,30 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  res.send(JSON.stringify({books},null,4))});
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  let isbn = req.params.isbn;
+  result = books[isbn-1];
+  res.send(JSON.stringify({result},null,4))
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+  const filteredBook = books.filter((book)=>book.author === author);
+  res.send(JSON.stringify({filteredBook},null,4))
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title = req.params.title;
+  const filteredBook = books.filter((book)=>book.title === title);
+  res.send(JSON.stringify({filteredBook},null,4))
 });
 
 //  Get book review
